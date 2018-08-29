d8eefec937cf2c34ad2f55f0e123fc44164a19a4
David <IshakDavids@gmail.com>
2018-08-29 13:13:50 +0700

    try 1

--- codingtrain1/sketch.js
+++ codingtrain1/sketch.js
@@ -1,4 +1,14 @@
-var y=0;
+var runcolour = {
+	r : 0,
+	g : 0,
+	b : 0
+};
+
+var paint = {
+	r : 0,
+	g : 0,
+	b : 0
+};
 
 var eye1 = {
 	x : 221,
@@ -16,21 +26,25 @@ var eye2 = {
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(100,100,5);
+	mouseX=240;
+	mouseY=115;
+	background(0);
 }
 
 function draw() {
 	//background(250,200,0);
-	y=y+1;
-	noStroke();
-	fill(100,10,y);
-	ellipse(400,50,16,32);
-
 	ellipseMode(CENTER);
 	rectMode(CENTER);
 
+	runcolour.r=random(0,255);
+	runcolour.g=random(0,255);
+	runcolour.b=runcolour.b+1;
+	noStroke();
+	fill(runcolour.r,runcolour.g,runcolour.b);
+	rect(500,115,50,50);
+
+	stroke(255,255,255);
 	//Body
-	stroke(0,0,0);
 	fill(255,0,0);
 	rect(240,145,20,100);
 	//Head
@@ -44,13 +58,18 @@ function draw() {
 	line(230,195,220,205);
 	line(250,195,260,205);
 
+	paint.r = map(mouseX,0,width,0,255);
+	paint.g = map(mouseY,0,height,255,0); // can be inverted
+	paint.b = 255-paint.r; // my way to invert
+	//text(col2,10,10);
 	noStroke();
-	fill(255,255,255,50);
+	fill(paint.r,paint.g,paint.b,50);
 	ellipse(mouseX,mouseY,20,20);
 
 	//console.log(mouseX);
 }
 
 function mousePressed() {
-	background(100,100,5);
+	// background(100,100,5);
+	background(0);
 }
