6bf5ff697ff0996d641ab4ff1fa54bd843fe15fa
David <IshakDavids@gmail.com>
2018-08-29 11:03:19 +0700

    update

--- codingtrain1/sketch.js
+++ codingtrain1/sketch.js
@@ -1,21 +1,36 @@
 var y=0;
 
+var eye1 = {
+	x : 221,
+	y : 115,
+	w : 16,
+	h : 32
+};
+
+var eye2 = {
+	x : 259,
+	y : 115,
+	w : 16,
+	h : 32
+};
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(250,200,0);
+	background(100,100,5);
 }
 
 function draw() {
 	//background(250,200,0);
 	y=y+1;
 	noStroke();
-	fill(100,y,10);
+	fill(100,10,y);
 	ellipse(400,50,16,32);
 
 	ellipseMode(CENTER);
 	rectMode(CENTER);
 
 	//Body
+	stroke(0,0,0);
 	fill(255,0,0);
 	rect(240,145,20,100);
 	//Head
@@ -23,8 +38,8 @@ function draw() {
 	ellipse(240,115,60,60);
 	//Eyes
 	fill(0,255,0);
-	ellipse(221,115,16,32);
-	ellipse(259,115,16,32);
+	ellipse(eye1.x,eye1.y,eye1.w,eye1.h);
+	ellipse(eye2.x,eye2.y,eye2.w,eye2.h);
 	//Legs
 	line(230,195,220,205);
 	line(250,195,260,205);
@@ -37,5 +52,5 @@ function draw() {
 }
 
 function mousePressed() {
-	background(250,200,0);
+	background(100,100,5);
 }
